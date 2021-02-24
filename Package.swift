2ff19da57e4f1bf103520568053415bad4bf2f91
Oleksii Moiseenko <>
2021-02-24 11:58:39 +0200

    made throwable repository;

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/techpro-studio/AbstractPersistence", from: "0.0.3"),
+        .package(url: "https://github.com/techpro-studio/AbstractPersistence", from: "0.0.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/CDPersistence/BaseCoreDataRepository.swift
+++ Sources/CDPersistence/BaseCoreDataRepository.swift
@@ -19,35 +19,35 @@ open class BaseCoreDataRepository<T:CDRepresentable>: AbstractRepository where T
         self.managedObjectContextFactory = managedObjectContextFactory
     }
 
-    public func save(value: T) {
+    open func save(value: T) throws {
         let managedObject = self.getManagedObjects(predicate: self.getIdPredicate(id: value.id)).first ?? create()
         value.update(value: managedObject)
-        try? self.managedObjectContext.save()
+        try self.managedObjectContext.save()
     }
 
-    public func create() -> T.CoreDataType {
+    open func create() -> T.CoreDataType {
         return NSEntityDescription.insertNewObject(forEntityName: T.CoreDataType.entityName, into: self.managedObjectContext) as! T.CoreDataType
     }
 
-    public func remove(value: T) {
-        self.remove(id: value.id)
+    open func remove(value: T) throws {
+        try self.remove(id: value.id)
     }
 
-    public func insert(value: T) {
+    open func insert(value: T) throws {
         let managedObject = create()
         value.update(value: managedObject)
         try? self.managedObjectContext.save()
     }
 
-    public func insertMany(value: [T]) {
-        value.forEach(self.insert)
+    open func insertMany(value: [T]) throws {
+        try value.forEach(self.insert)
     }
 
-    public func saveMany(value: [T]) {
-        value.forEach(self.save)
+    open func saveMany(value: [T]) throws {
+        try value.forEach(self.save)
     }
 
-    public func getById(id: T.Identifier) -> T? {
+    open func getById(id: T.Identifier) -> T? {
         return self.get(predicate: self.getIdPredicate(id: id))
     }
 
@@ -55,18 +55,19 @@ open class BaseCoreDataRepository<T:CDRepresentable>: AbstractRepository where T
         return NSPredicate(format: "\(T.CoreDataType.primaryKey)=\(id)")
     }
 
-    public func remove(id: T.Identifier) {
+    open func remove(id: T.Identifier) throws {
         guard let object = self.getManagedObjects(predicate: getIdPredicate(id: id)).first as? NSManagedObject  else {
             return
         }
         self.managedObjectContext.delete(object)
+        try self.managedObjectContext.save()
     }
 
-    public func find(request: FindRequest) -> [T] {
+    open func find(request: FindRequest) -> [T] {
         return self.getManagedObjects(predicate: request.predicate, sortDescriptors: request.sortDescriptors, skip:  request.skip, limit: request.limit).map { $0.asDomain() }
     }
 
-    private func getManagedObjects(predicate: NSPredicate, sortDescriptors: [NSSortDescriptor]?=nil, skip: Int?=nil, limit: Int?=nil) -> [T.CoreDataType] {
+    open func getManagedObjects(predicate: NSPredicate, sortDescriptors: [NSSortDescriptor]?=nil, skip: Int?=nil, limit: Int?=nil) -> [T.CoreDataType] {
         let request = NSFetchRequest<T.CoreDataType>(entityName: T.CoreDataType.entityName)
         request.predicate = predicate
         request.fetchOffset = skip ?? 0
@@ -75,7 +76,7 @@ open class BaseCoreDataRepository<T:CDRepresentable>: AbstractRepository where T
         return (try? self.managedObjectContext.fetch(request)) ?? []
     }
 
-    public func get(predicate: NSPredicate) -> T? {
+    open func get(predicate: NSPredicate) -> T? {
         return self.getManagedObjects(predicate: predicate).first?.asDomain()
     }
 }
